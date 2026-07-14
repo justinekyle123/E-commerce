ed04564250983316b316e1718645cbd6192d350a
justinekyle123 <justinekylenecesario5@gmail.com>
2026-07-15 00:17:13 +0800

    feat: design main homepage layout with header and footer components

--- resources/js/Components/Layout/Footer.tsx
+++ resources/js/Components/Layout/Footer.tsx
@@ -1,10 +1,56 @@
+import { Link } from "@inertiajs/react";
 
 
 const Footer = () => {
     return(
-        <footer>
-            <div>
-                <h1>footer</h1>
+        <footer className="bg-gray-900">
+            <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div className="grid gap-8 md:grid-cols-4">
+                    <div className="md:col-span-2">
+                        <Link 
+                        href={"/"}
+                        className="text-2xl font-bold text-white">
+                            KyleStore
+                        </Link>
+                        <p className="mx-2-md mt-4 text-sm text-gray-400">
+                            A modern e-commerce demo built with Laravel and React. Integrated with 
+                            HitPay for seamless payment processing.
+                        </p>
+                    </div>
+
+                    <div>
+                        <h3 className="mb-4 text-white font-semibold tracking-wider uppercase">
+                            Quick Links
+                        </h3>
+                        <ul className="space-y-3">
+                            <li>
+                                <Link
+                                href={"/"}
+                                className="text-gray-400 transition-colors text-sm 
+                                hover:text-white"> 
+                                    Home
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                href={"/"}
+                                className="text-gray-400 transition-colors text-sm 
+                                hover:text-white"> 
+                                    Products
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                href={"/"}
+                                className="text-gray-400 transition-colors text-sm 
+                                hover:text-white"> 
+                                    Cart
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
             </div>
         </footer>
     );
--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,10 +1,55 @@
 import MainLayout from "@/Components/Layout/MainLayout";
+import ProductGrid from "@/Components/Product/ProductGrid";
+import { Product } from "@/types";
+import { Head, Link } from "@inertiajs/react";
 import React from "react";
 
-const Home = () =>{
+interface HomeProps{
+    featuredProducts: Product[];
+    newProducts: Product[];
+}
+
+const Home = ({featuredProducts, newProducts}: HomeProps) =>{
     return (
         <MainLayout>
-            <h1>Hello</h1>
+            <Head title="Home" />
+            <section className="relative overflow-hidden bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900">
+                <div className="absolute inset-0 opacity-10">
+                    <div className="absolute inset-0 bg[linear-gradient(45deg, transparent_25%, 
+                    rgba(8,145,178,0,1)_50%, transparent_75%)]" />
+                </div>
+                <div className="relative px-4 py-24 mx-w max-w-7xl sm:px-6 lg:px-8 lg:py-32">
+                    <div className="max-w-2xl">
+                        <h1 className="text-4xl font-bold tracking-tight text-white
+                        sm:text-5xl lg:text-6xl">Simple <br />
+                            <span className="text-primary-400">E-commerce</span>
+                        </h1>
+                        <p className="mt-6 text-gray-300 text-lg">
+                            Discover our curated collection of premium clothing.
+                            Quality meets minimalist design.
+                        </p>
+                        <div className="flex flex-wrap gap-4 mt-8">
+                            <Link href="" className="inline-flex items-center gap-2 px-6 bg-primary-400
+                            rounded-lg text-white py-3 transition-colors hover:bg-primary-500">
+                            {""}
+                            Shop Now
+                            </Link>
+                            <Link href="" className="inline-flex items-center gap-2 px-6 bg-primary-400
+                            rounded-lg text-white py-3 transition-colors hover:bg-primary-500">
+                            {""}
+                            View Collection
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="px-4 py-16 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div className="flex items-center justify-between mb-">
+                    <h2 className="text-2xl font-bold text-gray-900">New Arrivals</h2>
+                    <ProductGrid products={newProducts}/>
+                </div>
+            </section>
         </MainLayout>
     );
 };
