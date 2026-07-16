59a697e40fcc87ca8e459313b101a094c5ab67a6
justine Kyle <justinekylenecesario5@gmail.com>
2026-07-16 12:42:24 +0800

    configured: AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,7 +21,10 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        Vite::prefetch(concurrency: 3);
+{
+    if (config('app.env') === 'production' || config('app.env') === 'staging') {
+        URL::forceScheme('https');
     }
+}
+
 }
